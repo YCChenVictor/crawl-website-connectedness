95350a6a44ea4bc29eebdb91ce01962f5b81fa28
YCChen <victor.yong.jhe.chen@gmail.com>
2024-06-01 15:43:37 +0800

    chore: Update package.json main file to index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Import the functions from the crawlWebsiteConnectedness.ts file
+import * as crawlWebsiteConnectedness from './crawlWebsiteConnectedness';
+
+// Export the functions
+export default crawlWebsiteConnectedness;
